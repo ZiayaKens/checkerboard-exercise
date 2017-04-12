895d92eb39dfd79b2d229c7924f72f0bf7d765a3
Jordan Klaers <jordanklaers@Lizs-MacBook-Pro.local>
2017-04-12 16:14:36 -0600

    made a red and black board

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+// console.log(typeof.body);
+function row(){
+  for(let i=0;i<63;i++){
+    if(i%2 == 0){
+      let newdiv = document.createElement('div');
+      newdiv.style.backgroundColor = 'black';
+      newdiv.style.float = 'left';
+      newdiv.style.paddingBottom = '11.1%';
+      newdiv.style.width = '11.1%';
+      body.appendChild(newdiv);
+    }
+    else {
+      let newdiv = document.createElement('div');
+      newdiv.style.backgroundColor = 'red';
+      newdiv.style.float = 'left';
+      newdiv.style.paddingBottom = '11.1%';
+      newdiv.style.width = '11.1%';
+      body.appendChild(newdiv);
+    }
+  }
+}// Your JS goes here
+
+row();
